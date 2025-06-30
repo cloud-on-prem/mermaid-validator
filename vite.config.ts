f2ea5dd5e0173dd909a665eaee6fb50db94b9668
Prem Pillai <prem@squareup.com>
2025-06-30 16:50:11 +1000

    chore: reduce bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       formats: ['es']
     },
     rollupOptions: {
-      external: ['mermaid', 'commander', 'chalk', 'fs', 'path'],
+      external: ['mermaid', 'commander', 'chalk', 'fs', 'path', 'marked'],
     },
     target: 'node18',
     outDir: 'dist',
